07e85489ab657940917cae6dc860d2791334fa04
idesiegramos <159062617+idesiegramos@users.noreply.github.com>
2024-07-31 20:13:12 +0200

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,3 +1,5 @@
+import streamlit as st
+
 st.title("hashajhjshjs")
 st.write(
     "EstaVIVA VENEZUELAn, solo debes escribir en el chat la información que quieres obtener, como por ejemplo un resumen con los puntos importantes de la clase o resolver otro tipo de dudas relacionadas con el contenido del vídeo. "
