aa42604d2fd4e514ddb399ba979cf6b12dd9d845
leirealvira <159037822+leirealvira@users.noreply.github.com>
2024-07-31 20:12:02 +0200

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,1 +1,6 @@
-oa coca cola
+st.title("hashajhjshjs")
+st.write(
+    "EstaVIVA VENEZUELAn, solo debes escribir en el chat la información que quieres obtener, como por ejemplo un resumen con los puntos importantes de la clase o resolver otro tipo de dudas relacionadas con el contenido del vídeo. "
+    "Te será de ayuda."
+)
+
