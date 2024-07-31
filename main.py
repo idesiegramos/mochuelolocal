3b06156a5fa24d536eac91cc93f49dd562fd0f61
idesiegramos <159062617+idesiegramos@users.noreply.github.com>
2024-07-31 19:39:51 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,187 @@
+import streamlit as st
+import os
+import tiktoken
+import openai
+import tempfile
+import whisper
+import pytube
+import re
+import pinecone
+
+
+from openai import OpenAI
+from dotenv import load_dotenv
+from tiktoken import encoding_for_model
+from langchain_openai.chat_models import ChatOpenAI
+from pytube import YouTube
+from youtube_transcript_api import YouTubeTranscriptApi
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import DocArrayInMemorySearch
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone, ServerlessSpec
+
+
+
+################################
+# Secretos
+################################
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+print("'Secretos' cargados correctamente")
+
+
+
+################################
+# Vídeo de prueba
+################################
+
+YOUTUBE_VIDEO_URL = "https://www.youtube.com/watch?v=dgZaIk3iFhc" # Clase MEP de IDESIE
+# YOUTUBE_VIDEO_URL = "https://www.youtube.com/watch?v=ROax8vdhuEQ"
+
+
+
+################################
+# Modelo
+################################
+
+model_name = "gpt-3.5-turbo"
+
+model = ChatOpenAI(
+    openai_api_key=OPENAI_API_KEY,
+    model=model_name,
+    temperature=0,
+    max_tokens=1000,
+    #top_p=1,
+    #frequency_penalty=0,
+    #presence_penalty=0.6
+)
+print(f"Modelo '{model_name}' cargado correctamente")
+
+
+
+################################
+# Transcripción con youtube-transcript-api
+################################
+
+def extract_video_id(url):
+    # Intenta extraer el ID del video de la URL estándar y corta
+    patterns = [
+        r'(?:https?://)?(?:www\.)?youtube\.com/watch\?v=([^&]+)',  # URL estándar
+        r'(?:https?://)?youtu\.be/([^?]+)'                         # URL corta
+    ]
+
+    for pattern in patterns:
+        match = re.search(pattern, url)
+        if match:
+            return match.group(1)
+
+    raise ValueError("URL de video no válida")
+
+# Reemplaza 'your_video_url' con la URL del video de YouTube
+video_url = YOUTUBE_VIDEO_URL
+YOUTUBE_VIDEO_ID = extract_video_id(video_url)
+print(f"ID del vídeo de YouTube: {YOUTUBE_VIDEO_ID}")
+
+
+def get_transcript(video_id):
+    try:
+        # Obtener la transcripción del video
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['es'])
+
+        # Concatenar las partes de la transcripción en un solo texto
+        transcript_text = "\n".join([entry['text'] for entry in transcript])
+        return transcript_text
+
+    except Exception as e:
+        return str(e)
+
+video_id = YOUTUBE_VIDEO_ID
+transcription_y = get_transcript(video_id)
+with open("./transcripts/transcription_y.txt", "w", encoding="utf-8") as file:
+            file.write(transcription_y)
+
+
+
+################################
+# Vector store
+################################
+
+
+
+
+
+###############################################################################
+# Construimos la página web
+###############################################################################
+
+
+# Título y descripción
+st.title("IA de IDESIE Business&Tech School")
+st.write(
+    "Esta Inteligencia Artificial te ayudará a con el contenido de las clases impartidas en IDESIE. "
+    "Para usar esta aplicación, solo debes escribir en el chat la información que quieres obtener, como por ejemplo un resumen con los puntos importantes de la clase o resolver otro tipo de dudas relacionadas con el contenido del vídeo. "
+    "Te será de ayuda."
+)
+import streamlit as st
+
+option = st.selectbox(
+    "Asignatura",
+    ("Fundamentos BIM", "REVIT ARQ", "REVIT MEP", "Management Skills"),
+)
+
+st.write("You selected:", option)
+
+st.video(f"{YOUTUBE_VIDEO_URL}")
+st.write(YOUTUBE_VIDEO_URL)
+
+with st.expander("Transcripción (primeros 1000 caracteres)"):
+     st.write(transcription_y[:1000])
+
+# Ask user for their OpenAI API key via `st.text_input`.
+# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+openai_api_key = OPENAI_API_KEY
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+else:
+
+    # Create an OpenAI client.
+    client = OpenAI(api_key=openai_api_key)
+
+    # Create a session state variable to store the chat messages. This ensures that the
+    # messages persist across reruns.
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display the existing chat messages via `st.chat_message`.
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # Create a chat input field to allow the user to enter a message. This will display
+    # automatically at the bottom of the page.
+    if prompt := st.chat_input("What is up?"):
+
+        # Store and display the current prompt.
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        # Generate a response using the OpenAI API.
+        stream = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+
+        # Stream the response to the chat using `st.write_stream`, then store it in 
+        # session state.
+        with st.chat_message("assistant"):
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
