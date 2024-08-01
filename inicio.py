98446465476611a8d7cc13cd9a9f686dc7866ab1
leirealvira <159037822+leirealvira@users.noreply.github.com>
2024-08-01 18:21:13 +0200

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,8 +1,13 @@
+st.logo(
+    LOGO_URL_LARGE,
+    link="https://streamlit.io/gallery",
+    icon_image=LOGO_URL_SMALL,
+)
+
 import streamlit as st
 
 st.title("IDE-IA")
 st.write(
-    "La Inteligencia Artificial de IDESIE....................."
-    "Te será de ayuda."
+    "Bienvenido a la Inteligencia Artificial de IDESIE"
+    "Busca, investiga, documenta"
 )
-
